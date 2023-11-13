7dd25765518a8053c4b7e5605a99e2071b320bd7
Sereti Kelvin <codedroidwithsereti@gmail.com>
2023-11-13 09:04:46 +0300

    Add files via upload

--- parking.py
+++ parking.py
@@ -1,24 +1,39 @@
-import random, math
-import numpy as np
-import scipy as sp
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import SimPy.Simulation as Sim
-class Arrival(Sim.Process):
-   """ Source generates cars at random
-Arrivals are at a time-dependent rate
-"""
-   def generate(self):
-       i=0
-       while (self.sim.now() < G.maxTime):
-           tnow = self.sim.now()
-           arrivalrate = 100 + 10 * math.sin(math.pi * tnow/12.0)
-           t = random.expovariate(arrivalrate)
-           yield Sim.hold, self, t
-           c = Car(name="Car%02d" % (i), sim=self.sim)
-           timeParking = random.expovariate(1.0/G.parkingtime)
-           self.sim.activate(c, c.visit(timeParking))
-           i += 1
+class ParkingLot:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.available_spaces = capacity
 
-        
-           
+    def park(self):
+        if self.available_spaces > 0:
+            print("Car parked. Available spaces:", self.available_spaces - 1)
+            self.available_spaces -= 1
+        else:
+            print("No available spaces. Parking lot is full.")
+
+    def leave(self):
+        if self.available_spaces < self.capacity:
+            print("Car left. Available spaces:", self.available_spaces + 1)
+            self.available_spaces += 1
+        else:
+            print("No cars in the parking lot.")
+
+def main():
+    capacity = 10  # You can set the parking lot capacity here
+    parking_lot = ParkingLot(capacity)
+
+    while True:
+        print("\n1. Park\n2. Leave\n3. Exit")
+        choice = input("Enter your choice (1/2/3): ")
+
+        if choice == '1':
+            parking_lot.park()
+        elif choice == '2':
+            parking_lot.leave()
+        elif choice == '3':
+            print("Exiting the simulation.")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
